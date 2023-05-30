d6dc8d0aacc75e479a24c11c80bc2095e123a8c4
Wilder Miranda <wildermiranda99@gmail.com>
2023-05-30 12:28:02 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const lamp = document.querySelector('.lamp')
+const light = document.querySelector('.lamp-light')
+
+lamp.addEventListener('click', _ => {
+    if (light.classList.contains('on')) {
+        light.classList.remove('on')
+        light.classList.add('off')
+    
+    } else {
+        light.classList.remove('off')
+        light.classList.add('on')
+    }
+})
